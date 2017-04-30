fe850d0d5ff638c7b72cbc5f4617d75457423a8a
moisei4 <moiseenko.eugene@gmail.com>
2017-04-30 15:16:58 +0300

    commit

--- header.php
+++ header.php
@@ -94,99 +94,97 @@ body_class($sticky_header);
 	</div>
 	<?php endif; ?>
 	
-	<div class="header_middle">
-		<div class="header-info">
+	<div class="header-info">
 
-			<!-- Search Form -->
-			<div class="header_search_wrap">
-				<a href="#" class="header_search_btn stockware_theme_icon_header_search" id="header-searchbtn"></a>
-				<div class="header_search">
-					<a href="#" class="header_search_close"></a>
-					<?php get_search_form(); ?>
-				</div>
+		<!-- Search Form -->
+		<div class="header_search_wrap">
+			<a href="#" class="header_search_btn stockware_theme_icon_header_search" id="header-searchbtn"></a>
+			<div class="header_search">
+				<a href="#" class="header_search_close"></a>
+				<?php get_search_form(); ?>
 			</div>
-			
-			<?php if ( class_exists( 'WooCommerce' ) ) : ?>
+		</div>
+		
+		<?php if ( class_exists( 'WooCommerce' ) ) : ?>
 
-			<!-- Personal Menu -->
-			<div class="header-personal">
-				<?php if (is_user_logged_in()) : ?>
-				<a class="header-gopersonal" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"></a>
-				<ul>
-					<?php if (!empty($flexity_options['compare']['id'])) : ?>
-					<li>
-						<a href="<?php echo ($flexity_options['compare']['id']) ? get_permalink($flexity_options['compare']['id']) : ''; ?>"><?php echo esc_html__('Compare list', 'flexity'); ?> <span><?php echo count($flexity_options['compare']['list'])?></span></a>
-					</li>
-					<?php endif; ?>
-					<?php if (!empty($flexity_options['wishlist']['id'])) : ?>
-					<li>
-						<a href="<?php echo ($flexity_options['wishlist']['id']) ? get_permalink($flexity_options['wishlist']['id']) : ''; ?>"><?php echo esc_html__('Wishlist', 'flexity'); ?> <span><?php echo esc_attr($flexity_options['wishlist']['count']); ?></span></a>
-					</li>
-					<?php endif; ?>
-					<li class="header-personal-cart">
-						<a href="<?php echo esc_url(WC()->cart->get_cart_url()); ?>"><?php echo esc_html__('Shopping Cart', 'flexity'); ?> <span><?php echo WC()->cart->get_cart_contents_count()?></span></a>
-					</li>
-					<li class="header-order">
-						<a href="<?php echo esc_url(WC()->cart->get_checkout_url()); ?>"><?php echo esc_html__('Checkout', 'flexity'); ?></a>
-					</li>
-					<li>
-						<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"><?php echo esc_html__('My Account', 'flexity'); ?></a>
-					</li>
-					<li>
-						<a href="<?php echo esc_url(wc_customer_edit_account_url()); ?>"><?php echo esc_html__('Settings', 'flexity'); ?></a>
-					</li>
-					<li>
-						<a href="<?php echo esc_url( wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) ) ); ?>"><?php echo esc_html__('Log out', 'flexity'); ?></a>
-					</li>
-				</ul>
-				<?php else : ?>
-				<a class="header-gopersonal" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"></a>
-				<ul>
-					<li>
-						<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"><?php esc_html_e('Login / Register','flexity'); ?></a>
-					</li>
-				</ul>
+		<!-- Personal Menu -->
+		<div class="header-personal">
+			<?php if (is_user_logged_in()) : ?>
+			<a class="header-gopersonal" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"></a>
+			<ul>
+				<?php if (!empty($flexity_options['compare']['id'])) : ?>
+				<li>
+					<a href="<?php echo ($flexity_options['compare']['id']) ? get_permalink($flexity_options['compare']['id']) : ''; ?>"><?php echo esc_html__('Compare list', 'flexity'); ?> <span><?php echo count($flexity_options['compare']['list'])?></span></a>
+				</li>
 				<?php endif; ?>
-			</div>
-
-			<!-- Small Cart -->
-			<div class="header_cart_wrap">
-				<a href="<?php echo esc_url(WC()->cart->get_cart_url()); ?>" class="header-cart">
-					<div class="header-cart-inner">
-						<span class="header-cart-count">
-							<i class="stockware_theme_icon_header_cart"></i>
-							<span><?php echo WC()->cart->get_cart_contents_count()?></span>
-						</span>
-						<span class="header-cart-summ"><?php echo WC()->cart->get_cart_total(); ?></span>
-					</div>
-				</a>
-			</div>
-			
+				<?php if (!empty($flexity_options['wishlist']['id'])) : ?>
+				<li>
+					<a href="<?php echo ($flexity_options['wishlist']['id']) ? get_permalink($flexity_options['wishlist']['id']) : ''; ?>"><?php echo esc_html__('Wishlist', 'flexity'); ?> <span><?php echo esc_attr($flexity_options['wishlist']['count']); ?></span></a>
+				</li>
+				<?php endif; ?>
+				<li class="header-personal-cart">
+					<a href="<?php echo esc_url(WC()->cart->get_cart_url()); ?>"><?php echo esc_html__('Shopping Cart', 'flexity'); ?> <span><?php echo WC()->cart->get_cart_contents_count()?></span></a>
+				</li>
+				<li class="header-order">
+					<a href="<?php echo esc_url(WC()->cart->get_checkout_url()); ?>"><?php echo esc_html__('Checkout', 'flexity'); ?></a>
+				</li>
+				<li>
+					<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"><?php echo esc_html__('My Account', 'flexity'); ?></a>
+				</li>
+				<li>
+					<a href="<?php echo esc_url(wc_customer_edit_account_url()); ?>"><?php echo esc_html__('Settings', 'flexity'); ?></a>
+				</li>
+				<li>
+					<a href="<?php echo esc_url( wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) ) ); ?>"><?php echo esc_html__('Log out', 'flexity'); ?></a>
+				</li>
+			</ul>
+			<?php else : ?>
+			<a class="header-gopersonal" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"></a>
+			<ul>
+				<li>
+					<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"><?php esc_html_e('Login / Register','flexity'); ?></a>
+				</li>
+			</ul>
 			<?php endif; ?>
-
-			<?php if (!empty($flexity_options['compare']['id'])) : ?>
-			<a href="<?php echo (!empty($flexity_options['compare']['id'])) ? get_permalink($flexity_options['compare']['id']) : ''; ?>" class="header-compare"><?php if (count($flexity_options['compare']['list'])) : ?><span><?php echo count($flexity_options['compare']['list'])?></span><?php endif; ?></a>
-			<?php endif; ?>
-			<?php if (!empty($flexity_options['wishlist']['id'])) : ?>
-			<a href="<?php echo (!empty($flexity_options['wishlist']['id'])) ? get_permalink($flexity_options['wishlist']['id']) : ''; ?>" class="header_favorites"><?php if (!empty($flexity_options['wishlist']['count'])) : ?><span><?php echo esc_attr($flexity_options['wishlist']['count']); ?></span><?php endif; ?></a>
-			<?php endif; ?>
-
 		</div>
 
-		<!-- Navmenu - start -->
-		<?php
-		wp_nav_menu( array(
-			'theme_location' => 'rw-top-menu',
-			'container' => 'nav',
-			'container_class' => '',
-			'container_id' => 'top-menu',
-			'items_wrap' => '<ul id="navigation">%3$s</ul>',
-			'link_before' => 		'<span class="menu_item_wrap">', 
-			'link_after' => 		'</span>', 
-		) );
-		?>
-		<!-- Navmenu - end -->
+		<!-- Small Cart -->
+		<div class="header_cart_wrap">
+			<a href="<?php echo esc_url(WC()->cart->get_cart_url()); ?>" class="header-cart">
+				<div class="header-cart-inner">
+					<span class="header-cart-count">
+						<i class="stockware_theme_icon_header_cart"></i>
+						<span><?php echo WC()->cart->get_cart_contents_count()?></span>
+					</span>
+					<span class="header-cart-summ"><?php echo WC()->cart->get_cart_total(); ?></span>
+				</div>
+			</a>
+		</div>
+		
+		<?php endif; ?>
+
+		<?php if (!empty($flexity_options['compare']['id'])) : ?>
+		<a href="<?php echo (!empty($flexity_options['compare']['id'])) ? get_permalink($flexity_options['compare']['id']) : ''; ?>" class="header-compare"><?php if (count($flexity_options['compare']['list'])) : ?><span><?php echo count($flexity_options['compare']['list'])?></span><?php endif; ?></a>
+		<?php endif; ?>
+		<?php if (!empty($flexity_options['wishlist']['id'])) : ?>
+		<a href="<?php echo (!empty($flexity_options['wishlist']['id'])) ? get_permalink($flexity_options['wishlist']['id']) : ''; ?>" class="header_favorites"><?php if (!empty($flexity_options['wishlist']['count'])) : ?><span><?php echo esc_attr($flexity_options['wishlist']['count']); ?></span><?php endif; ?></a>
+		<?php endif; ?>
+
 	</div>
+
+	<!-- Navmenu - start -->
+	<?php
+	wp_nav_menu( array(
+		'theme_location' => 'rw-top-menu',
+		'container' => 'nav',
+		'container_class' => '',
+		'container_id' => 'top-menu',
+		'items_wrap' => '<ul id="navigation">%3$s</ul>',
+		'link_before' => 		'<span class="menu_item_wrap">', 
+		'link_after' => 		'</span>', 
+	) );
+	?>
+	<!-- Navmenu - end -->
 	
 	<div class="header_right">
 		<a href="#" class="button">Purchase</a>
